b8b2a55d25a932dca0b583ea601974997b1db1cb
Brian <brianizzo93@gmail.com>
2025-03-26 22:26:24 +0900

    added login form

---
+++ app/users/login/page.tsx
@@ -0,0 +1,20 @@
+import { redirect } from "next/navigation"
+import { Login } from "../../utils/userRequests"
+
+export default function loginForm() {
+	return (
+		<div>
+			<form action={async (formData) => {
+				'use server'
+				await Login(formData)
+				redirect('/')
+			}}>
+				<input type="email" name="email" id="email" placeholder="email@email.com" />
+				<br />
+				<input type="password" name="password" id="password" placeholder="password" />
+				<br />
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	)
+}
