b91b574513b73197493460cafd54d212897f5f66
Brian <brianizzo93@gmail.com>
2025-03-26 22:25:56 +0900

    added store card

---
+++ app/components/StoreCard.tsx
@@ -0,0 +1,12 @@
+
+
+export default function StoreCard({ entry }) {
+	return (
+	  <div className="border p-4 rounded-lg shadow-md bg-yellow-50">
+		<h2 className="text-lg font-semibold">{entry.name} 🍽️</h2>
+		<p><strong>Location:</strong> {entry.location}</p>
+		<p><strong>Date of Founding:</strong> {entry.date_of_founding}</p>
+		<p><strong>Updated:</strong> {new Date(entry.date_updated).toLocaleString()}</p>
+	  </div>
+	);
+  }
