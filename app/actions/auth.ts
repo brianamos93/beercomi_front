616e8b6eaec2310f9a5cc6ed1ec2b0323ebe5848
Brian <brianizzo93@gmail.com>
2025-03-26 22:29:30 +0900

    added auth actions file

---
+++ app/actions/auth.ts
@@ -0,0 +1,19 @@
+import { SignupFormSchema, FormState } from '../utils/def'
+import { Signup } from '../utils/userRequests'
+ 
+export async function signup(state: FormState, formData: FormData) {
+  // Validate form fields
+  const validatedFields = SignupFormSchema.safeParse({
+    display_name: formData.get('display_name'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+ 
+  // If any form fields are invalid, return early
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
+  Signup(formData)
+}
