3094a979c5d90bf72b96be122b08deecc1893815
Brian <brianizzo93@gmail.com>
2025-03-26 22:26:10 +0900

    added store overview

---
+++ app/stores/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import StoreCard from "../components/StoreCard";
+import { getStores } from "../utils/storeRequests"
+import { Store } from "../utils/def";
+
+export default async function stores() {
+	const data = await getStores()
+	return (
+			<main>
+				<div className="max-w-2xl mx-auto p-4">
+					<h1 className="text-2xl font-bold mb-4">Stores</h1>
+					<ul className="space-y-4">
+					{data.map((store: Store) => (
+						<li key={store.id}>
+							<Link href={`/stores/${store.id}`}>
+							<StoreCard entry={store} />
+							</Link>
+						</li>
+					))}
+					</ul>
+				</div>
+			</main>
+	)
+}
