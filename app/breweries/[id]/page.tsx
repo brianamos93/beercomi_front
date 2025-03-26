dc7c05653f99823914cda1945d073275e5feff6e
Brian <brianizzo93@gmail.com>
2025-03-26 22:24:43 +0900

    added single brewery page

---
+++ app/breweries/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { getBrewery, getBreweriesList } from "@/app/utils/breweryRequests";
+import { Brewery } from "@/app/utils/def";
+
+export async function generateStaticParams() {
+	const breweries = await getBreweriesList()
+
+	return breweries.map((brewery: Brewery) => ({
+		slug: brewery.id
+	}))
+}
+
+export default async function BreweryPage({params}:{params: Promise<{ id: string }> 
+}) {
+	const brewery = await getBrewery((await params).id)
+	return (
+			<main>
+				<div className="max-w-2xl mx-auto p-4">
+					<h1 className="text-2xl font-bold mb-4">{brewery.name}</h1>
+					<ul>
+						<li>{brewery.location}</li>
+						<li>{brewery.date_of_founding}</li>
+						<li>{brewery.authorid}</li>
+						<li>{brewery.display_name}</li>
+					</ul>
+				</div>
+			</main>
+	)	
+}
