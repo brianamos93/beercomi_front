3bc5a7bfb2617e76e2896621b1ba474b6a31b6ce
Brian <brianizzo93@gmail.com>
2025-03-26 22:24:29 +0900

    added breweries overview page

---
+++ app/breweries/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import BreweryCard from "../components/BreweryCard";
+import { getBreweries } from "../utils/breweryRequests";
+import { Brewery } from "../utils/def";
+
+export default async function brewerys() {
+	const data = await getBreweries()
+	return (
+			<main>
+				<div className="max-w-2xl mx-auto p-4">
+					<h1 className="text-2xl font-bold mb-4">Breweries</h1>
+					<ul className="space-y-4">
+					{data.map((brewery: Brewery) => (
+						<li key={brewery.id}>
+							<Link href={`/breweries/${brewery.id}`}>
+							<BreweryCard entry={brewery} />
+							</Link>
+						</li>
+					))}
+					</ul>
+				</div>
+			</main>
+	)
+}
