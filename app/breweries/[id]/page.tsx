d299f906955c4adfcd16dd35be39f596d1b88ce8
Brian <brianizzo93@gmail.com>
2025-07-02 22:54:01 +0900

    added edit link on brewery pages conditionaly rendered

--- app/breweries/[id]/page.tsx
+++ app/breweries/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { getBrewery, getBreweriesList } from "@/app/utils/requests/breweryRequests";
 import { Brewery } from "@/app/utils/def";
+import Link from "next/link";
+import { decrypt } from "@/app/utils/requests/userRequests";
+import { cookies } from "next/headers";
 
 export async function generateStaticParams() {
 	const breweries = await getBreweriesList()
@@ -11,6 +14,16 @@ export async function generateStaticParams() {
 
 export default async function BreweryPage({params}:{params: Promise<{ id: string }> 
 }) {
+	const session = await (await cookies()).get('session')?.value
+	let currentUserId = null
+	if(session) {
+		const decryptedCookie = await decrypt(session)
+		currentUserId = decryptedCookie.userID
+	} else {
+		currentUserId = null
+
+	}
+
 	const brewery = await getBrewery((await params).id)
 	return (
 			<main>
@@ -21,6 +34,11 @@ export default async function BreweryPage({params}:{params: Promise<{ id: string
 						<li>{brewery.date_of_founding}</li>
 						<li>{brewery.authorid}</li>
 						<li>{brewery.display_name}</li>
+						{brewery.authorid === currentUserId && (
+							<li><Link
+							href={`/breweries/${brewery.id}/edit`}
+							className="text-blue-600 hover:underline font-semibold">Edit</Link></li>
+						)}
 					</ul>
 				</div>
 			</main>
