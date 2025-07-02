abd608fbfb8c494906b914b6b180559208b08368
Brian <brianizzo93@gmail.com>
2025-07-02 22:53:22 +0900

    added edit brewery page

---
+++ app/breweries/[id]/edit/page.tsx
@@ -0,0 +1,24 @@
+import EditBreweryForm from "@/app/components/EditBreweryForm";
+import { getBrewery } from "@/app/utils/requests/breweryRequests";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+export const metadata: Metadata = {
+	title: 'Edit Brewery'
+}
+
+export default async function editBreweryPage(props: { params: Promise<{ id: string }> }) {
+	const params = await props.params;
+	const id = params.id
+	const brewery = await getBrewery(id)
+
+	if (!brewery) {
+		notFound()
+	}
+
+	return (
+		<main>
+			<EditBreweryForm brewery={brewery} />
+		</main>
+	)
+}
