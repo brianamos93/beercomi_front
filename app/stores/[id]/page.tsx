749a252356a6cc2df35faa0e01b918f7f853c6cf
Brian <brianizzo93@gmail.com>
2025-07-07 21:26:43 +0900

    add link to edit form page

--- app/stores/[id]/page.tsx
+++ app/stores/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { getStore, getStoresList } from "@/app/utils/requests/storeRequests";
 import { Store } from "@/app/utils/def";
+import { decrypt } from "@/app/utils/requests/userRequests";
+import Link from "next/link";
+import { cookies } from "next/headers";
 
 export async function generateStaticParams() {
 	const stores = await getStoresList()
@@ -11,6 +14,15 @@ export async function generateStaticParams() {
 
 export default async function StorePage({params}:{params: Promise<{ id: string }> 
 }) {
+	const session = await (await cookies()).get('session')?.value
+	let currentUserId = null
+		if(session) {
+			const decryptedCookie = await decrypt(session)
+			currentUserId = decryptedCookie.userID
+		} else {
+			currentUserId = null
+	
+		}
 	const store = await getStore((await params).id)
 	return (
 			<main>
@@ -21,6 +33,11 @@ export default async function StorePage({params}:{params: Promise<{ id: string }
 						<li>{store.date_of_founding}</li>
 						<li>{store.owner}</li>
 						<li>{new Date(store.date_updated).toLocaleString()}</li>
+						{store.author === currentUserId && (
+							<li><Link
+							href={`/beers/${store.id}/edit`}
+							className="text-blue-600 hover:underline font-semibold">Edit</Link></li>
+						)}
 					</ul>
 				</div>
 			</main>
