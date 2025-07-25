eaf2b0c676314a3402783d66b3ec3ad113347a18
Brian <brianizzo93@gmail.com>
2025-07-26 00:49:48 +0900

    changed brewery page to reflect author_id

--- app/breweries/[id]/page.tsx
+++ app/breweries/[id]/page.tsx
@@ -35,7 +35,7 @@ export default async function BreweryPage({params}:{params: Promise<{ id: string
 						<li>{brewery.date_of_founding}</li>
 						<li>{brewery.author_id}</li>
 						<li>{brewery.display_name}</li>
-						{brewery.authorid === currentUserId && (
+						{brewery.author_id === currentUserId && (
 							<li><Link
 							href={`/breweries/${brewery.id}/edit`}
 							className="text-blue-600 hover:underline font-semibold">Edit</Link></li>
