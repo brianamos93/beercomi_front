9f591ee358fee11eda25dc2c67bcbe7a2f5c8def
Brian <brianizzo93@gmail.com>
2025-08-06 22:26:30 +0900

    added profile img url

--- app/components/interface/NavCTA.tsx
+++ app/components/interface/NavCTA.tsx
@@ -6,23 +6,27 @@ export default async function NavCTA() {
 
 	let session = null
 	let displayName = null
+	let profile_img_url = null
 		//const token = (await cookies()).get('session')?.value;
 		const cookie = (await cookies()).get('session')?.value
 		if(cookie) {
 			const decryptedCookie = await decrypt(cookie)
 			session = decryptedCookie.token
 			displayName = decryptedCookie.displayName
+			profile_img_url = decryptedCookie.profile_img_url
 		}
 	
 	if (!session) {
 		const user = {
 			display_name: "Guest",
+			profile_img_url: "./public/defaultavatar.png",
 			isAuthenticated: false,
 		}
 		return <Header user={user} />
 	} else {
 	const user = {
 		display_name: displayName,
+		profile_img_url: profile_img_url,
 		isAuthenticated: Boolean(session),
 	}
 
