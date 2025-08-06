e0f6775d536475245fddf3ff536e4f443f48fede
Brian <brianizzo93@gmail.com>
2025-08-06 22:24:57 +0900

    added backend image to config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://placehold.jp/****')],
+    remotePatterns: [new URL('https://placehold.jp/****'), new URL('http://localhost:3005/uploads/****')],
   }
 };
 
