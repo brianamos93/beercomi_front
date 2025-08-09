c368c7bf66f8f54a4833663c0e13693f18ba7791
Brian <brianizzo93@gmail.com>
2025-08-09 17:23:33 +0900

    added hero icons

--- app/users/profile/page.tsx
+++ app/users/profile/page.tsx
@@ -1,4 +1,5 @@
 import { decrypt, getOneUser } from "@/app/utils/requests/userRequests"
+import { PlusIcon } from "@heroicons/react/24/solid"
 import { cookies } from "next/headers"
 import Image from 'next/image'
 import Link from "next/link"
@@ -17,21 +18,28 @@ export default async function Profile() {
 	const pictureurl = user.profile_img_url ? `http://localhost:3005/uploads/${user.profile_img_url}` : "http://localhost:3005/uploads/defaultavatar.png";
 	const altText = user.profile_img_url ? `${user.display_name}'s avatar` : "Default Avatar";
 	return (
-		<main>
-			<div>
-				<div>
-					<Image
-					src={pictureurl}
-					alt={altText}
-					height={200}
-					width={200}
-					className="rounded-full max-w-full h-auto"
-					/>
+		<main className="flex flex-col">
+			<div className="flex flex-row justify-center p-4">
+				<div className="relative">
+					<div>
+						<Image
+						src={pictureurl}
+						alt={altText}
+						height={100}
+						width={100}
+						className="rounded-full max-w-full h-auto"
+						/>
+					</div>
+					<div className="absolute right-0 bottom-0">
+						<Link href="/users/profile/edit">
+						<PlusIcon className="size-8 text-white rounded-full bg-gray-600"/>
+						</Link>
+					</div>
 				</div>
-				<Link href="/users/profile/edit"
-				>
-					Edit
-				</Link>
+				<div className="p-4">
+					<h2 className="text-3xl">{user.display_name}</h2>
+					<p className="">Osaka, Japan</p>
+				</div>
 			</div>
 		</main>
 	)
