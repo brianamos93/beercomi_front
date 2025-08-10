0b9df68008d32949ea2a2659a0380e1c811e5750
Brian <brianizzo93@gmail.com>
2025-08-10 23:20:10 +0900

    changed reference to reveiw data

--- app/components/beer/review/ReveiwCard.tsx
+++ app/components/beer/review/ReveiwCard.tsx
@@ -1,32 +1,14 @@
-import { Beer, Review } from "@/app/utils/def";
-import { getBeer } from "@/app/utils/requests/beerRequests";
-import { useEffect, useState } from "react";
-
+import { Review } from "@/app/utils/def";
 
 export default function ReviewCard({ entry }: { entry: Review}) {
-	const [beer, setBeer] = useState<Beer>()
-
-	useEffect(() => {
-		async function fetchData() {
-			const res = await getBeer(entry.beer_id)
-			setBeer(res)
-		}
-		fetchData()
-	}, [entry.beer_id])
 	return (
 	  <div className="border p-4 rounded-lg shadow-md bg-yellow-50">
-    	{beer ? (
-			<>
 				<h2 className="text-lg font-semibold">
-					{beer.brewery_name}&apos;s {beer.name} Review
+					{entry.brewery_name}&apos;s {entry.beer_name} Review
 				</h2>
         		<p><strong>Author:</strong> {entry.author_display_name}</p>
         		<p><strong>Rating:</strong> {entry.rating}</p>
         		<p><strong>Updated:</strong> {new Date(entry.date_updated).toLocaleString()}</p>
-      		</>
-    ) : (
-      <p>Loading...</p>
-    )}
   </div>
 	);
   }
