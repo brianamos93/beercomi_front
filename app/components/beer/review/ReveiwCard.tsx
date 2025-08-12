559b0f6d04914742c1ade9c42782cfd2d8dfa03e
Brian <brianizzo93@gmail.com>
2025-08-12 16:30:07 +0900

    made link one level lower and replaced author_display_name with author_name

--- app/components/beer/review/ReveiwCard.tsx
+++ app/components/beer/review/ReveiwCard.tsx
@@ -3,17 +3,19 @@ import Link from "next/link";
 
 export default function ReviewCard({ entry }: { entry: Review}) {
 	return (
-	<Link
-	href={`/beers/${entry.beer_id}#${entry.id}`}
-	>
-	  <div className="border p-4 rounded-lg shadow-md bg-yellow-50">
-				<h2 className="text-lg font-semibold">
-					{entry.brewery_name}&apos;s {entry.beer_name} Review
-				</h2>
-        		<p><strong>Author:</strong> {entry.author_display_name}</p>
-        		<p><strong>Rating:</strong> {entry.rating}</p>
-        		<p><strong>Updated:</strong> {new Date(entry.date_updated).toLocaleString()}</p>
-  	</div>
-	</Link>
+		<>
+			<Link
+			href={`/beers/${entry.beer_id}#${entry.id}`}
+			>
+			<div className="border p-4 rounded-lg shadow-md bg-yellow-50">
+						<h2 className="text-lg font-semibold">
+							{entry.brewery_name}&apos;s {entry.beer_name} Review
+						</h2>
+						<p><strong>Author:</strong> {entry.author_name}</p>
+						<p><strong>Rating:</strong> {entry.rating}</p>
+						<p><strong>Updated:</strong> {new Date(entry.date_updated).toLocaleString()}</p>
+			</div>
+			</Link>
+		</>
 	);
   }
