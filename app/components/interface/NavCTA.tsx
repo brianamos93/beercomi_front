2d97fd1dab9aa60e36586fafddc6224dfc396330
Brian <brianizzo93@gmail.com>
2025-08-12 16:30:31 +0900

    Replaced decrypt with getLoggenInUsersData

--- app/components/interface/NavCTA.tsx
+++ app/components/interface/NavCTA.tsx
@@ -1,22 +1,21 @@
-import { decrypt } from "../../utils/requests/userRequests";
+import { getLoggedInUsersData } from "@/app/utils/requests/userRequests";
 import Header from "./Header";
 import { cookies } from "next/headers";
 
 export default async function NavCTA() {
 
-	let session = null
 	let displayName = null
 	let profile_img_url = null
 		//const token = (await cookies()).get('session')?.value;
-		const cookie = (await cookies()).get('session')?.value
-		if(cookie) {
-			const decryptedCookie = await decrypt(cookie)
-			session = decryptedCookie.token
-			displayName = decryptedCookie.displayName
-			profile_img_url = decryptedCookie.profile_img_url
+		const token = (await cookies()).get('token')?.value
+		if(token) {
+			const userData = await getLoggedInUsersData(token)
+		
+			displayName = userData.display_name
+			profile_img_url = userData.profile_img_url
 		}
 	
-	if (!session) {
+	if (!token) {
 		const user = {
 			display_name: "Guest",
 			profile_img_url: "./public/defaultavatar.png",
@@ -27,7 +26,7 @@ export default async function NavCTA() {
 	const user = {
 		display_name: displayName,
 		profile_img_url: profile_img_url,
-		isAuthenticated: Boolean(session),
+		isAuthenticated: Boolean(token),
 	}
 
 	return <Header user={user} />
