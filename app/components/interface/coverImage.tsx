d4fa6cac5b4d7c9176e3026f122e5716ca1614a0
Brian <brianizzo93@gmail.com>
2025-08-25 00:07:41 +0900

    added cover_image component

---
+++ app/components/interface/coverImage.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function CoverImage({cover_image, name}: {cover_image: string, name: string}) {
+	const coverImage = cover_image
+	? `http://localhost:3005/${cover_image}`
+    	: "/file.svg";
+	return (
+			<Image
+			src={coverImage}
+			width={200}
+			height={200}
+			alt={`${name}`}
+			className="object-cover rounded-t-lg"
+			/>
+	)
+}
