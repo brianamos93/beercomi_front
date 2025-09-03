0e521cfdb33cae05b185767d39c58c7dbb24f8db
Brian <brianizzo93@gmail.com>
2025-09-03 23:21:44 +0900

    added remote image routes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://placehold.jp/****'), new URL('http://localhost:3005/uploads/****')],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.jp",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3005",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
