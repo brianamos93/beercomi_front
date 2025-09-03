8a8494688e45b8ddee6a05c6b07346c1a56424cd
Brian <brianizzo93@gmail.com>
2025-09-03 23:20:13 +0900

    added file upload to createbeerreview

--- app/components/beer/review/CreateBeerReviewForm.tsx
+++ app/components/beer/review/CreateBeerReviewForm.tsx
@@ -15,8 +15,17 @@ export default function CreateBeerReviewForm({
 		<form action={formAction}>
 			<input type="hidden" name="beer_id" value={beer.id} />
 			<div>
+				<label htmlFor="photos">Upload photos:</label>
+				<input type="file" name="photos" id="photos" accept="image/png image/jpeg" multiple />
+				<div id="photos-error" aria-live="polite" aria-atomic="true">
+					{state?.errors?.photos && state.errors.photos.map((error: string) => (
+						<p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
+					))}
+				</div>
+			</div>
+			<div>
 				<label htmlFor="review">Review:</label>
-				<textarea name="review" id="review" cols="30" rows="10" defaultValue={state?.review?.review}></textarea>
+				<textarea name="review" id="review" cols={30} rows={10} defaultValue={state?.review?.review}></textarea>
 				<div id="review-error" aria-live="polite" aria-atomic="true">
 						{state?.errors?.review && state.errors.review.map((error: string) => (
 							<p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
