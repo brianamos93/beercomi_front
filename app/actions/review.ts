d2e3c976836ae3c9a5d91eefc98c8400859883d7
Brian <brianizzo93@gmail.com>
2025-09-22 22:40:29 +0900

    changed server function to include messages, errors, and status code

--- app/actions/review.ts
+++ app/actions/review.ts
@@ -1,128 +1,81 @@
-'use server'
+"use server";
 
-import { z } from "zod"
-import { cookies } from "next/headers"
-import { createReview, editReview } from "../utils/requests/reviewRequests"
-import { revalidatePath } from "next/cache"
-import { redirect } from "next/navigation"
+import { cookies } from "next/headers";
+import { createReview, editReview } from "../utils/requests/reviewRequests";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import z from "zod";
 
-const fileSchema = z.custom<File>();
-
-const ReviewFormSchema = z.object({
-	id: z.string(),
-	review: z.string()
-	.trim()
-	.min(10, {message: 'Review must be at least 10 character long.'}),
-	rating: z.coerce.number(),
-	date_updated: z.date(),
-	date_created: z.date(),
-	beer_id: z.string(),
-	author_id: z.string(),
-	photos: z
-    .union([fileSchema, z.array(fileSchema)])
-    .transform((val) => (Array.isArray(val) ? val : [val]))
-    .refine((arr) => arr.length <= 4, {
-      message: "Up to 4 photos are allowed.",
-    }),
-})
-
-export type State = {
-	review?:{
-		review?: string | null;
-		rating?: number | null;
-		photos?: File[] | null;
-	}
-	errors?: {
-		review?: string[];
-		rating?: string[];
-		photos?: string[];
-	}
-	message?: string | null;
-	}
-
-const CreateReview = ReviewFormSchema.omit({ id: true, date_updated: true, date_created: true, author_id: true })
-const UpdateReview = ReviewFormSchema.omit({ id: true, date_updated: true, date_created: true, author_id: true })
-
-export async function createServerReview(prevState: State, formData: FormData) {
-		const token = (await cookies()).get('token')?.value
-
-		const validatedFields = CreateReview.safeParse({
-			beer_id: formData.get('beer_id'),
-			review: formData.get('review'),
-			rating: formData.get('rating'),
-			photos: formData.get('photos')
-		})
-
-		if (token == undefined) {
-			return {
-				errors: "Not Logged In"
-			}
-		}
-
-		if (!validatedFields.success) {
-			return {
-				review: {
-					review: formData.get('review')?.toString() ?? '',
-					rating: Number(formData.get('rating')) ?? '',
-					photos: formData.get('photos')?.toString() ?? ''
-				},
-				errors: validatedFields.error.flatten().fieldErrors,
-				message: 'Missing Fields. Failed to Create Review.'
-			}
-		}
-		try {
-			await createReview(formData, token)
-		} catch (error) {
-			return {
-				message: 'Database Error: Failed to Create Review.'
-			}
-		}
-		revalidatePath('/beers')
-		redirect(`/beers/${formData.get('beer_id')}`)
+export async function transformZodErrors(error: z.ZodError) {
+	return error.issues.map((issue) => ({
+		path: issue.path.join("."),
+		message: issue.message,
+	}));
 }
 
-export async function editServerReview(
-	id: string, 
-	prevState: State, 
-	formData: FormData,
-) {
+export async function createServerReview(formData: FormData) {
+	const token = (await cookies()).get("token")?.value;
+	const beerId = formData.get("beer_id");
 
-	const token = (await cookies()).get('token')?.value
-
-
-	const validatedFields = UpdateReview.safeParse({
-		beer_id: formData.get('beer_id'),
-		review: formData.get('review'),
-		rating: formData.get('rating'),
-		photos: formData.get('photos')
-	})
-
-	if (token == undefined) {
-		return {
-			errors: "Not Logged In"
-		}
+	if (!token) {
+		return { error: "Not Logged In", status: 401 };
 	}
 
-	if (!validatedFields.success) {
-		return {
-			review: {
-				review: formData.get('review')?.toString() ?? '',
-				rating: Number(formData.get('rating')) ?? '',
-				photos: formData.get('photos') ?? ''
-			},
-			errors: validatedFields.error.flatten().fieldErrors,
-			message: 'Missing Fields. Failed to Edit Review.'
-		}
-	}
 	try {
-		await editReview(id, formData, token)
-	} catch (error) {
-		return {
-			message: 'Database Error: Failed to Edit Review.'
+		const res = await createReview(formData, token);
+
+		if (res.error) {
+			return {
+				error: res.error,
+				status: res.status ?? 400,
+			};
 		}
-		
+		if (res.message) {
+      revalidatePath(`/beers/${beerId}`);
+      redirect(`/beers/${beerId}`);
+    }
+    return {
+      error: "Unexpected API response",
+      status: 500,
+    };
+	} catch (err: unknown) {
+		console.error("Server action error:", err);
+    return {
+      error: "Unexpected server error",
+      status: 500,
+    };
 	}
-	
-	revalidatePath('/beers')
-	redirect(`/beers/${formData.get('beer_id')}`)
+}
+
+export async function editServerReview(id: string, formData: FormData) {
+	const token = (await cookies()).get("token")?.value;
+	const beerId = formData.get("beer_id");
+	if (!token) {
+		return { error: "Not Logged In", status: 401 };
+	}
+
+	try {
+		const res = await editReview(id, formData, token);
+
+		if (res.error) {
+			return {
+				error: res.error,
+				status: res.status ?? 400,
+			};
+		}
+		if (res.message) {
+      revalidatePath(`/beers/${beerId}`);
+      redirect(`/beers/${beerId}`);
+    }
+    return {
+      error: "Unexpected API response",
+      status: 500,
+    };
+	} catch (err: unknown) {
+		console.error("Server action error:", err);
+    return {
+      error: "Unexpected server error",
+      status: 500,
+    };
+	}
 }
