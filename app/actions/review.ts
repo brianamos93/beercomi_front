12e5b7f53c3fff9d8e96a63b4a8ea884069e786f
Brian <brianizzo93@gmail.com>
2025-10-06 22:02:31 +0900

    moved revalidatePath and redirect to outside the try catch

--- app/actions/review.ts
+++ app/actions/review.ts
@@ -20,62 +20,61 @@ export async function createServerReview(formData: FormData) {
 	if (!token) {
 		return { error: "Not Logged In", status: 401 };
 	}
+	let res;
 
 	try {
-		const res = await createReview(formData, token);
-
-		if (res.error) {
-			return {
-				error: res.error,
-				status: res.status ?? 400,
-			};
-		}
-		if (res.message) {
-      revalidatePath(`/beers/${beerId}`);
-      redirect(`/beers/${beerId}`);
-    }
-    return {
-      error: "Unexpected API response",
-      status: 500,
-    };
+		res = await createReview(formData, token);
 	} catch (err: unknown) {
 		console.error("Server action error:", err);
-    return {
-      error: "Unexpected server error",
-      status: 500,
-    };
+		return {
+			error: "Unexpected server error",
+			status: 500,
+		};
 	}
+	if (res.error) {
+		return {
+			error: res.error,
+			status: res.status ?? 400,
+		};
+	}
+	if (res.message) {
+		revalidatePath(`/beers/${beerId}`);
+		redirect(`/beers/${beerId}`);
+	}
+
+	return { error: "Unexpected API response", status: 500 };
 }
 
 export async function editServerReview(id: string, formData: FormData) {
 	const token = (await cookies()).get("token")?.value;
 	const beerId = formData.get("beer_id");
+	console.log(formData)
 	if (!token) {
 		return { error: "Not Logged In", status: 401 };
 	}
+	let res;
 
 	try {
-		const res = await editReview(id, formData, token);
-
-		if (res.error) {
-			return {
-				error: res.error,
-				status: res.status ?? 400,
-			};
-		}
-		if (res.message) {
-      revalidatePath(`/beers/${beerId}`);
-      redirect(`/beers/${beerId}`);
-    }
-    return {
-      error: "Unexpected API response",
-      status: 500,
-    };
+		res = await editReview(id, formData, token);
 	} catch (err: unknown) {
 		console.error("Server action error:", err);
-    return {
-      error: "Unexpected server error",
-      status: 500,
-    };
+		return {
+			error: "Unexpected server error",
+			status: 500,
+		};
 	}
+	if (res.error) {
+		return {
+			error: res.error,
+			status: res.status ?? 400,
+		};
+	}
+	if (res.message) {
+		revalidatePath(`/beers/${beerId}`);
+		redirect(`/beers/${beerId}`);
+	}
+	return {
+		error: "Unexpected API response",
+		status: 500,
+	};
 }
