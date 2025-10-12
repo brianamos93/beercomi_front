deef8b98d4c94cb221841158d859a77aab7bf339
Brian <brianizzo93@gmail.com>
2025-10-12 20:17:19 +0900

    added transformZodErrors to utils

--- app/utils/utils.tsx
+++ app/utils/utils.tsx
@@ -1,3 +1,12 @@
+import z from "zod";
+
 const url = "http://localhost:3005"
 
 export default url
+
+export async function transformZodErrors(error: z.ZodError) {
+	return error.issues.map((issue) => ({
+		path: issue.path.join("."),
+		message: issue.message,
+	}));
+}
