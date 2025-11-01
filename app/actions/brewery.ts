071a3c5fd670a1afe32fff26f794e0aba5a52696
Brian <brianizzo93@gmail.com>
2025-11-01 11:39:56 +0900

    refactor brewery server action for use with reacthookform

--- app/actions/brewery.ts
+++ app/actions/brewery.ts
@@ -1,112 +1,75 @@
-'use server'
+"use server";
 
-import { z } from "zod"
 import { cookies } from "next/headers";
-import { createBrewery, updateBrewery } from "../utils/requests/breweryRequests";
+import {
+	createBrewery,
+	updateBrewery,
+} from "../utils/requests/breweryRequests";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-const BreweryFormSchema = z.object({
-	id: z.string(),
-	name: z
-	.string().trim()
-	.min(1, { message: 'Brewery name must be at least 1 character long.'}).trim(),
-	location: z.string().trim().min(5, { message: 'Brewery location must be at least 5 characters long.'}),
-	date_of_founding: z.string().trim().min(4, { message: 'The date of founding must be at least 4 character long.'}),
-	date_updated: z.date(),
-	date_created: z.date(),
-	cover_image: z.any()
-})
+export async function createServerBrewery(formData: FormData) {
+	const token = (await cookies()).get("token")?.value;
 
-export type State = {
-	brewery?: {
-		name?: string | null;
-		location?: string | null;
-		date_of_founding: string | null;
-		cover_image: File | null;
-	};
-	errors?: {
-		name?: string[];
-		location?: string[];
-		date_of_founding: string[];
-		cover_iamge: string[];
-	};
-	message?: string | null;
-}
-
-const CreateBrewery = BreweryFormSchema.omit({ id: true, date_updated: true, date_created: true})
-const UpdateBrewery = BreweryFormSchema.omit({ id: true, date_updated: true, date_created: true})
-
-export async function createServerBrewery(prevState: State, formData: FormData) {
-		const token = (await cookies()).get('token')?.value
-
-		const validatedFields = CreateBrewery.safeParse({
-			name: formData.get('name'),
-			location: formData.get('location'),
-			date_of_founding: formData.get('date_of_founding'),
-			cover_image: formData.get('cover_image')
-		})
-
-		if (token == undefined) {
-			return {
-				errors: "Not Logged In"
-			}
-		}
-
-		if (!validatedFields.success) {
-			return {
-				brewery: {
-					name: formData.get('name')?.toString() ?? '',
-					location: formData.get('location')?.toString() ?? '',
-					date_of_founding: formData.get('date_of_founding')?.toString() ?? '',
-					cover_image: formData.get('cover_image')?.toString() ?? '',
-				},
-				errors: validatedFields.error.flatten().fieldErrors,
-				message: 'Missing Fields. Failed to Create Brewery.'
-			}
-		}
-
-		try {
-			await createBrewery(formData, token)
-		} catch (error) {
-			return {
-				message: 'Datebase Error: Failed to Create Brewery.'
-			}
-		}
-
-		revalidatePath('/breweries')
-		redirect('/breweries')
-}
-
-export async function updateServerBrewery(
-	id: string,
-	prevState: State,
-	formData: FormData,
-) {
-
-	const token = (await cookies()).get('token')?.value
-
-
-	const validatedFields = UpdateBrewery.safeParse({
-		name: formData.get('name'),
-		location: formData.get('location'),
-		date_of_founding: formData.get('date_of_founding'),
-		cover_image: formData.get('cover_image'),
-	})
-
-	if (token == undefined) {
-		return {
-			errors: "Not Logged In"
-		}
+	if (!token) {
+		return { error: "Not Logged In", status: 401 };
 	}
 
-	if (!validatedFields.success) {
+	let res;
+
+	try {
+		res = await createBrewery(formData, token);
+	} catch (error) {
+		console.log("Server action error:", error);
 		return {
-			errors: validatedFields.error.flatten().fieldErrors,
-			message: 'Missing Fields. Failed to update brewery.',
-		}
+			error: "Unexpected server error",
+			status: 500,
+		};
 	}
-	await updateBrewery(id, formData, token)
-	revalidatePath('/breweries')
-	redirect('/breweries')
+
+	if (res.error) {
+		return {
+			error: res.error,
+			status: res.status ?? 400,
+		};
+	}
+	if (res.id) {
+		revalidatePath("/breweries");
+		redirect(`/breweries/${res.id}`);
+	}
+	return { error: "Unexpected API response", status: 500 };
+}
+
+export async function updateServerBrewery(id: string, formData: FormData) {
+	const token = (await cookies()).get("token")?.value;
+
+	if (!token) {
+		return { error: "Not Logged In", status: 401 };
+	}
+	let res;
+
+	try {
+		res = await updateBrewery(id, formData, token);
+	} catch (error) {
+		console.error("Server action error:", error);
+		return {
+			error: "Unexpected server error",
+			status: 500,
+		};
+	}
+	if (res.error) {
+		return {
+			error: res.error,
+			status: res.status ?? 400,
+		};
+	}
+	if (res.id) {
+		revalidatePath("/breweries");
+		redirect("/breweries");
+	}
+
+	return {
+		error: "Unexpected API response",
+		status: 500,
+	};
 }
