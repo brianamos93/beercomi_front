df487e3d5a7abba1de9553e53f30f2a81a72b948
Brian <brianizzo93@gmail.com>
2025-11-01 11:36:46 +0900

    refactor editbeerschema

--- app/utils/schemas/beerSchema.ts
+++ app/utils/schemas/beerSchema.ts
@@ -53,26 +53,7 @@ export const CreateBeerSchema = z.object({
 		
 })
 
-export const EditBeerSchema = z.object({
-	name: z
-		.string()
-		.trim()
-		.min(1, "Name is required."),
-	style: z
-		.string()
-		.trim()
-		.min(1, "Style is required."),
-	abv: z.coerce.number().min(0, "ABV must be a positive number."),
-	brewery_id: z
-		.string()
-		.min(1, "A brewery is required."),
-	color: z
-		.string()
-		.min(1, "Color is required."),
-	ibu: z.coerce.number().min(0, "IBU must be a positive number."),
-	description: z
-		.string()
-		.min(1, "Description is required."),
+export const EditBeerSchema = CreateBeerSchema.extend({
 	cover_image: z.union([newEditCoverImageSchema, existingCoverImageSchema]).nullable().optional(),
 	deleteCoverImage: z.boolean()
 		
