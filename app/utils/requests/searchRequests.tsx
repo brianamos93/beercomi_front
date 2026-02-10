b60000d22ce49d39ca7e6b16ceb21ea4dce08677
Brian <brianizzo93@gmail.com>
2026-02-10 16:31:47 +0900

    fixed search query

--- app/utils/requests/searchRequests.tsx
+++ app/utils/requests/searchRequests.tsx
@@ -1,7 +1,7 @@
 import url from "../utils"
 
-export const getSearch = async (query: string, limit: string, offset: string) => {
-	const res = await fetch(`${url}/search?query=${query}&limit=${limit}&offset=${offset}`,
+export const getSearch = async (query: string, limit: number, offset: number) => {
+	const res = await fetch(`${url}/search?q=${query}&limit=${limit}&offset=${offset}`,
 		{ cache: "no-store" }
 	)
 	if (!res.ok) throw new Error("Failed to fetch search results.")
