6a0a3aca798984594c7c5a3b97342503a9fca3c8
Brian <brianizzo93@gmail.com>
2026-03-02 21:44:51 +0900

    added conditional image

--- app/components/beer/BeerCardDetailed.tsx
+++ app/components/beer/BeerCardDetailed.tsx
@@ -1,9 +1,16 @@
 import { Beer } from "@/app/utils/def";
+import Image from "next/image";
+import { NewspaperIcon } from "@heroicons/react/24/outline";
+
 
 export default function BeerCardDetailed({beer}: {beer:Beer}) {
 	return (
 		<div>
 		<h1 className="text-2xl font-bold mb-4">{beer.name}</h1>
+		{beer.cover_image ? (
+			<Image src={beer.cover_image} alt={beer.name} width={200} height={200} />): (
+				<NewspaperIcon className="w-48 h-48 text-gray-500"/>
+		)}
 			<ul>
 				<li>{beer.brewery_name}</li>
 				<li>{beer.description}</li>
