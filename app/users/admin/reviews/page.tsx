c59304ce508f815bef2407b128a2ddc2d7cb5f05
Brian <brianizzo93@gmail.com>
2026-03-06 13:17:04 +0900

    added review admin page

---
+++ app/users/admin/reviews/page.tsx
@@ -0,0 +1,13 @@
+import ReviewTable from "@/app/components/interface/admin/ReviewTable"
+import { requireToken } from "@/app/utils/libs/token"
+import { cookies } from "next/headers"
+
+export default async function beerTable() {
+	const token = requireToken(await ((await cookies()).get("token")?.value))
+
+	return (
+		<div>
+			<ReviewTable token={token} />
+		</div>
+	)
+}
