13c01d00214fd197d047aade63cb25de94a0f23c
Brian <brianizzo93@gmail.com>
2026-03-06 18:35:22 +0900

    fixed get individual brewery request

--- app/utils/requests/breweryRequests.tsx
+++ app/utils/requests/breweryRequests.tsx
@@ -1,60 +1,76 @@
 import { DeletedFilter } from "../def";
-import url from "../utils"
+import url from "../utils";
 
-export const getBreweries = async ({ limit, offset, q }: { limit: number; offset: number; q?: string }) => {
-  let queryString = "limit=" + limit + "&offset=" + offset;
-  if (q) {
-    queryString += "&q=" + encodeURIComponent(q);
-  }
-  const res = await fetch(
-    url + "/breweries?" + queryString,
-    { cache: "no-store" }
-  );
+export const getBreweries = async ({
+	limit,
+	offset,
+	q,
+}: {
+	limit: number;
+	offset: number;
+	q?: string;
+}) => {
+	let queryString = "limit=" + limit + "&offset=" + offset;
+	if (q) {
+		queryString += "&q=" + encodeURIComponent(q);
+	}
+	const res = await fetch(url + "/breweries?" + queryString, {
+		cache: "no-store",
+	});
 
-  if (!res.ok) throw new Error("Failed to fetch breweries");
+	if (!res.ok) throw new Error("Failed to fetch breweries");
 
-  return res.json();
+	return res.json();
 };
 
-
 export const getBreweriesOneUser = async (id: string) => {
-	const res = await fetch(`${url}/user/${id}/breweries`)
-	return res.json()
-}
+	const res = await fetch(`${url}/user/${id}/breweries`);
+	return res.json();
+};
 
 export const getBreweriesList = async () => {
-	const res = await fetch(url + '/breweries/list')
-	return res.json()
-}
+	const res = await fetch(url + "/breweries/list");
+	return res.json();
+};
 
-export const getBrewery = async (id: string) => {
-	const res = await fetch(`${url}/breweries/${id}`)
-	return res.json()
-}
+export const getBrewery = async (id: string, limit: number, offset: number) => {
+	const res = await fetch(
+		`${url}/breweries/${id}?limit=${limit}&offset=${offset}`,
+		{
+			cache: "no-store",
+		},
+	);
 
-export const createBrewery = async (newBreweryData:FormData, token: string) => {
-	const res = await fetch(url + '/breweries', {
+	return res.json();
+};
+export const createBrewery = async (
+	newBreweryData: FormData,
+	token: string,
+) => {
+	const res = await fetch(url + "/breweries", {
 		method: "POST",
 		headers: {
 			Authorization: `Bearer ${token}`,
-
 		},
-		body: newBreweryData
-	})
-	return res.json()
-}
+		body: newBreweryData,
+	});
+	return res.json();
+};
 
-export const updateBrewery = async (id: string, updatedBreweryData: FormData, token: string) => {
-	const res = await fetch(`${url}/breweries/${id}`,{
+export const updateBrewery = async (
+	id: string,
+	updatedBreweryData: FormData,
+	token: string,
+) => {
+	const res = await fetch(`${url}/breweries/${id}`, {
 		method: "PUT",
 		headers: {
 			Authorization: `Bearer ${token}`,
-
 		},
-		body: updatedBreweryData
-	})
-	return res.json()
-}
+		body: updatedBreweryData,
+	});
+	return res.json();
+};
 
 export const deleteBrewery = async (id: string, token: string) => {
 	await fetch(`${url}/breweries/${id}`, {
@@ -62,68 +78,73 @@ export const deleteBrewery = async (id: string, token: string) => {
 		headers: {
 			Authorization: `Bearer ${token}`,
 			"Content-Type": "application/json",
-		}
-	})
-}
-
-export const adminBreweryGet = async ( {token, limit, offset, deleted}: {token: string, limit: string, offset: string, deleted: DeletedFilter}) => {
-	const res = await fetch(url + `/breweries/admin/view?limit=${limit}&offset=${offset}&deleted=${deleted}`, {
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${token}`,
-
 		},
-	})
-	return res.json()
-}
+	});
+};
+
+export const adminBreweryGet = async ({
+	token,
+	limit,
+	offset,
+	deleted,
+}: {
+	token: string;
+	limit: string;
+	offset: string;
+	deleted: DeletedFilter;
+}) => {
+	const res = await fetch(
+		url +
+			`/breweries/admin/view?limit=${limit}&offset=${offset}&deleted=${deleted}`,
+		{
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		},
+	);
+	return res.json();
+};
 
 export const favoriteBreweries = async (token: string) => {
 	const res = await fetch(`${url}/favorites/brewereies`, {
 		method: "GET",
 		headers: {
-			Authorization: `Bearer ${token}`
+			Authorization: `Bearer ${token}`,
 		},
-	})
-	return res.json()
-}
+	});
+	return res.json();
+};
 
 export const softDeleteBrewery = async (id: string, token: string) => {
-	const res = await fetch(
-		url + "/breweries/" + id,
-		{
-			method: "DELETE",
-			headers: {
-				Authorization: `Bearer ${token}`
-			},
-		}
-	)
-	if(!res.ok) throw new Error("Failed to soft delete beer")
-	return res.json()
-}
-
-export const hardDeleteBrewery = async (id: string, token: string) => {
-	const res = await fetch(
-		url + "/breweries/admin/hard-delete/" + id,
-		{
+	const res = await fetch(url + "/breweries/" + id, {
 		method: "DELETE",
 		headers: {
-			Authorization: `Bearer ${token}`
-		}
-		}
-	)
-	if(!res.ok) throw new Error("Failed to hard delete beer")
-	return res.json()
-}
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	if (!res.ok) throw new Error("Failed to soft delete beer");
+	return res.json();
+};
 
-export const undoSoftDeleteBrewery = async(id: string, token: string) => {
-	const res = await fetch(
-		url + "/breweries/admin/undo/delete/" + id, {
-			method: "PUT",
-			headers: {
-				Authorization: `Bearer ${token}`
-			},
-		}
-	)
-	if(!res.ok) throw new Error("Failed to undo soft delete")
-	return res.json()
-}
+export const hardDeleteBrewery = async (id: string, token: string) => {
+	const res = await fetch(url + "/breweries/admin/hard-delete/" + id, {
+		method: "DELETE",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	if (!res.ok) throw new Error("Failed to hard delete beer");
+	return res.json();
+};
+
+export const undoSoftDeleteBrewery = async (id: string, token: string) => {
+	const res = await fetch(url + "/breweries/admin/undo/delete/" + id, {
+		method: "PUT",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	if (!res.ok) throw new Error("Failed to undo soft delete");
+	return res.json();
+};
