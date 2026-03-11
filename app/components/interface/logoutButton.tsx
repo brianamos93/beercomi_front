c03619428afd14070631bfcc11d91ab3c9693665
Brian <brianizzo93@gmail.com>
2026-03-11 16:50:15 +0900

    changed style for logout button

--- app/components/interface/logoutButton.tsx
+++ app/components/interface/logoutButton.tsx
@@ -2,8 +2,13 @@ import { logout } from "@/app/utils/requests/userRequests";
 
 export default function LogoutButton() {
 	return (
-		    <form action={logout}>
-                <button type="submit">Logout</button>
-            </form>
-	)
+		<form action={logout}>
+			<button
+				type="submit"
+				className="text-gray-700 hover:text-red-600"
+			>
+				Logout
+			</button>
+		</form>
+	);
 }
