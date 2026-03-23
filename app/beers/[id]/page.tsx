719d1a6c73438ed53bed3e0b3def47f44b331690
Brian <brianizzo93@gmail.com>
2026-03-23 16:15:30 +0900

    added ssg

--- app/beers/[id]/page.tsx
+++ app/beers/[id]/page.tsx
@@ -4,12 +4,20 @@ import { Suspense } from "react";
 import BeerReviewsSection from "./components/BeerReviewsSection";
 import BeerDynamicSection from "./components/BeerDynamicSection";
 import { notFound } from "next/navigation";
-import { getBeer } from "@/app/utils/requests/beerRequests";
+import { getBeer, getBeersList } from "@/app/utils/requests/beerRequests";
 import { SearchParams } from "next/dist/server/request/search-params";
+import { Beer } from "@/app/utils/def";
+
+export async function generateStaticParams() {
+  const beers = await getBeersList()
+  return beers.map((beer: Beer) => ({
+    id: beer.id
+  }))
+}
 
 export default async function BeerPage({ params, searchParams }: {params: {id: string}, searchParams: SearchParams}) {
   const { id } = await params;
-  const beer = await getBeer(id); // SSG — fast
+  const beer = await getBeer(id);
   if (!beer) notFound();
 
   const searchParamsPage = await searchParams;
