6225cf66cf4dda81c6975a5d932986f045ad6e2d
Brian <brianizzo93@gmail.com>
2026-03-24 21:30:37 +0900

    fix params

--- app/beers/[id]/edit/page.tsx
+++ app/beers/[id]/edit/page.tsx
@@ -4,28 +4,34 @@ import { getBeer } from "@/app/utils/requests/beerRequests";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata({ params }: {params: Beer}): Promise<Metadata> {
-	const { id } = await params
-  const beer = await getBeer(id);
+export async function generateMetadata({
+	params,
+}: {
+	params: Beer;
+}): Promise<Metadata> {
+	const { id } = await params;
+	const beer = await getBeer(id);
 
-  return {
-	title: `${beer.name}の情報を編集`,
-	description: `${beer.name}の味わいや特徴、スタイル、カラーなどを簡単に修正可能。`,
-  };
+	return {
+		title: `${beer.name}の情報を編集`,
+		description: `${beer.name}の味わいや特徴、スタイル、カラーなどを簡単に修正可能。`,
+	};
 }
 
-export default async function editBeerPage(props: { params: Promise<{ id: string }> }) {
+export default async function editBeerPage(props: {
+	params: Promise<{ id: string }>;
+}) {
 	const params = await props.params;
-	const id = params.id
+	const id = params.id;
 	const beer = await getBeer(id);
 
 	if (!beer) {
-		notFound()
+		notFound();
 	}
 
 	return (
 		<main>
 			<EditBeerForm beer={beer} />
 		</main>
-	)
+	);
 }
--- app/breweries/[id]/edit/page.tsx
+++ app/breweries/[id]/edit/page.tsx
@@ -4,28 +4,34 @@ import { getBrewery } from "@/app/utils/requests/breweryRequests";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata({ params }: {params: Brewery}): Promise<Metadata> {
-	const { id } = await params
-  const brewery = await getBrewery(id);
+export async function generateMetadata({
+	params,
+}: {
+	params: Brewery;
+}): Promise<Metadata> {
+	const { id } = await params;
+	const brewery = await getBrewery(id);
 
-  return {
-	title: `${brewery.name}の情報を編集`,
-	description: `${brewery.name}の住所、設立年などを簡単に修正可能。`,
-  };
+	return {
+		title: `${brewery.name}の情報を編集`,
+		description: `${brewery.name}の住所、設立年などを簡単に修正可能。`,
+	};
 }
 
-export default async function editBreweryPage(props: { params: Promise<{ id: string }> }) {
+export default async function editBreweryPage(props: {
+	params: Promise<{ id: string }>;
+}) {
 	const params = await props.params;
-	const id = params.id
-	const brewery = await getBrewery(id)
+	const id = params.id;
+	const brewery = await getBrewery(id);
 
 	if (!brewery) {
-		notFound()
+		notFound();
 	}
 
 	return (
 		<main>
 			<EditBreweryForm brewery={brewery} />
 		</main>
-	)
+	);
 }
