8e0c7e0100da99587d65b0658f56f231afdff512
Brian <brianizzo93@gmail.com>
2026-03-24 18:50:03 +0900

    translated pagination base

--- app/components/interface/paginationBase.tsx
+++ app/components/interface/paginationBase.tsx
@@ -19,30 +19,30 @@ export function PaginationUI({
     <div className="flex items-center justify-between mt-4">
       {previousHref ? (
         <a href={previousHref} className="px-3 py-1 border rounded">
-          Previous
+          前のページ
         </a>
       ) : previousAction ? (
         <button onClick={previousAction} className="px-3 py-1 border rounded">
-          Previous
+          前のページ
         </button>
       ) : (
-        <span className="px-3 py-1 border rounded opacity-50">Previous</span>
+        <span className="px-3 py-1 border rounded opacity-50">前のページ</span>
       )}
 
       <span>
-        Page {currentPage} of {totalPages}
+        {currentPage}/{totalPages}ページ
       </span>
 
       {nextHref ? (
         <a href={nextHref} className="px-3 py-1 border rounded">
-          Next
+          次のページ
         </a>
       ) : nextAction ? (
         <button onClick={nextAction} className="px-3 py-1 border rounded">
-          Next
+          次のページ
         </button>
       ) : (
-        <span className="px-3 py-1 border rounded opacity-50">Next</span>
+        <span className="px-3 py-1 border rounded opacity-50">次のページ</span>
       )}
     </div>
   );
